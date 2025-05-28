2d3c654a251362e27c31fa54e6a9c1bbeb13aef9
arthit.s <arthit.sekmoh@gmail.com>
2025-05-28 10:13:56 +0700

    Update queue_data.py

--- queue_data.py
+++ queue_data.py
@@ -3,19 +3,23 @@ import time
 import os
 
 TXT_DIR = "/app/txt"
+
 os.makedirs(TXT_DIR, exist_ok=True)
 r = redis.Redis(host='redis', port=6379, db=0)
-
 def write_from_queue():
     while True:
         item = r.blpop('data_queue', timeout=5)
         if item:
             _, data = item
             data = data.decode('utf-8')
-            mc_no = data.split('_')[-1]
-            filename = f"{TXT_DIR}/file-{mc_no}.txt"
+            lines = data.splitlines()
+            if not lines:
+                continue
+            line_no = lines[0].strip()
+            filename = f"{TXT_DIR}/PC-{line_no}.txt"
 
-            with open(filename, "a", encoding="utf-8") as f: 
+            # with open(filename, "a", encoding="utf-8") as f: # append
+            with open(filename,"w",encoding="utf-8") as f: # not append
                 f.write(data + "\n")
                 print(f"record to {filename}: {data}")
         else:
